f3ffb91c92f50bfb99e3a5433d03e73a46cfb996
Adam Jenkins <jenkinsa9@udayton.edu>
2025-02-03 13:47:48 -0500

    Update CodeForPayload.ino

--- CodeForPayload.ino
+++ CodeForPayload.ino
@@ -1,17 +1,20 @@
 #include <LW20.h>
 #include <lw20api.h>
-LW20 lw20(Serial1, 115200);               // Instantiate the LW20 using the hardware Serial Port1 at 115200 baud
+
+LW20 lw20(Serial1, 115200); // Instantiate the LW20 using the hardware Serial Port1 at 115200 baud
 const int Motoroutput = 9;  
 const int MotorReverse = 10;
+
 void setup() 
 {
   pinMode(Motoroutput, OUTPUT);
   pinMode(MotorReverse, OUTPUT);
+  
   // Start serial monitor port.
-  Serial.begin(115200);                 // Prepare the USB Terminal to use as a monitor
+  Serial.begin(115200); // Prepare the USB Terminal to use as a monitor
 
   // Setup LW20.
-  lw20.init();                          // Get the LW20 ready for commands
+  lw20.init(); // Get the LW20 ready for commands
   lw20.setLaserParams(LWMS_48);
 }
 
