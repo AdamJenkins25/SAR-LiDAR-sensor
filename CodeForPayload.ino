4c07226476fffcb29eff0e4a91b37e86562c80ff
BryceBontrager <bontragerb1@udayton.edu>
2025-02-07 13:03:39 -0500

    Update CodeForPayload.ino

--- CodeForPayload.ino
+++ CodeForPayload.ino
@@ -18,15 +18,35 @@ void lw20Connect() {
   // Send some characters to make sure the LW20 switches to serial mode.
   LW20_SERIAL.print('www\r\n');
 }
-void loop() 
-{
+void loop() {
+  USER_SERIAL.println("LW20 sample code started");
+  
+  // Connect to the LW20.
+  lw20Connect();
 
- float lw20GetDistance() {
+  // Show product information to show a valid connection is established.
+  lw20PrintProductInfo();
+
+  // Run a loop that asks for distance every 25ms.
+  while (true) {
+    float distance = lw20GetDistance();
+    float strength = lw20GetStrength();
+    
+    USER_SERIAL.print("Distance: ");
+    USER_SERIAL.print(distance);
+    USER_SERIAL.print(" m Strength: ");
+    USER_SERIAL.print(strength);
+    USER_SERIAL.println(" %");
+    
+    delay(25);
+  }
+}
+
+}
+float lw20GetDistance() {
   // NOTE: LDL = median distance to last return.
   if (lw20SendCommand("?ldl\r\n"))
     return getNumberFromResponse(responseData);
 
   return 0.0f;
-}
-
 }
