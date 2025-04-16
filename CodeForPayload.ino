97689de4d23c0e1a20d38e288c09fb67cbd539c6
Adam Jenkins <jenkinsa9@udayton.edu>
2025-04-16 12:22:21 -0400

    Update CodeForPayload.ino

--- CodeForPayload.ino
+++ CodeForPayload.ino
@@ -1,166 +1,59 @@
 //-------------------------------------------------------------------------------------------
-// LightWare LW20/SF20 Arduino LWNX serial connection sample.
-// https://lightware.co.za
+// Please note that this sample needs an Arduino board that supports at least 2 hardware
+// UARTs. If you are using an Uno you can use the software serial library instead of the
+// Serial1 hardware UART.
 //-------------------------------------------------------------------------------------------
-// This sample connects to the LW20/SF20 with the advanced binary protocol.
-// The Arduino Uno only has a single hardware serial port, and therefore this sample
-// has been modified to use a software serial port that can be used on custom
-// defined pins.
-//-------------------------------------------------------------------------------------------
+int motorForward = A2;
+int motorReverse = A3;
 
-#include <SoftwareSerial.h>
-SoftwareSerial LW20_SERIAL(0, 1); // RX, TX
-
-#define USER_SERIAL           Serial
-#define USER_SERIAL_BAUDRATE  115200
-#define LW20_BAUDRATE         38400
-#define MOTOR_PIN             9       // Motor control pin
-#define TRIGGER_DISTANCE      1.0     // Distance threshold in meters
-
-// LW20 response string data.
-char responseData[40];
-int responseDataSize = 0;
-unsigned long lastReadTime = 0;
-const unsigned long READ_INTERVAL = 25; // Read every 25ms
-
-//----------------------------------------------------------------------------------------------
-// Application setup.
-//----------------------------------------------------------------------------------------------
 void setup() {
-  pinMode(MOTOR_PIN, OUTPUT);
-  digitalWrite(MOTOR_PIN, LOW); // Ensure motor is off initially
+  pinMode(motorForward, OUTPUT);
+  pinMode(motorReverse, OUTPUT);
 
-  USER_SERIAL.begin(USER_SERIAL_BAUDRATE); // Connect to the serial console
-  LW20_SERIAL.begin(LW20_BAUDRATE);
+  Serial.begin(115200);
 
-  USER_SERIAL.println("LW20 sample sode started");
-  lw20Connect();
-  lw20PrintProductInfo();
+  // Baud rate of the device can be configured.
+  Serial1.begin(115200);
+
+  Serial1.write("\r\n");
+  while (Serial1.available()) {
+    Serial1.read();
+  }
 }
 
-//----------------------------------------------------------------------------------------------
-// Application main loop.
-//----------------------------------------------------------------------------------------------
 void loop() {
-  if(millis() - lastReadTime >= READ_INTERVAL) {
-    lastReadTime = millis();
-    float distance = lw20GetDistance();
-    float strength = lw20GetStrength();
-
-    if (distance >= 0) {
-      USER_SERIAL.print("Distance: ");
-      USER_SERIAL.print(distance);
-      USER_SERIAL.print(" m Strength: ");
-      USER_SERIAL.print(strength);
-      USER_SERIAL.println(" %");
-
-      // Motor activation logic
-      if (disatnce < TRIGGER_DISTANCE) {
-        digitalWrite(MOTOR_PIN, HIGH); // Activate motor
-      } else {
-        digitalWrite(MOTOR_PIN, LOW); // Deactivate motor
-      }
-    } else {
-      USER_SERIAL.println("Error reading distance");
-    }
-  }
-}
-
-//----------------------------------------------------------------------------------------------
-// Conenct to the LW20 and enable serial mode.
-//----------------------------------------------------------------------------------------------
-void lw20Connect() {
-  LW20_SERIAL.print('www\r\n'); // Send some characters to make sure the LW20 switches to serial mode
-  delay(500); // Allow time for mode switch
-}
-
-//----------------------------------------------------------------------------------------------
-// Send a command string and wait for a response. Returns true if a response was received.
-// The response string will be held in 'responseData'.
-//----------------------------------------------------------------------------------------------
-bool lw20SendCommand(const char* CommandStr) {
-  // Make sure there is no unwanted data left in the buffer.
-  while (LW20_SERIAL.available()) {
-    LW20_SERIAL.read();
-  }
-
-  LW20_SERIAL.print(CommandStr); // Send the command.
-
-  unsigned long timeoutTime = millis() + 1000; // Only have 1 second timeout.
-  responseDataSize = 0;
+  char resultBuffer[8];
+  int bufferIndex = 0;
+  int c = 0;
   
-  // Wait for the full response.
-  while (millis() < timeoutTime) {
-    if (LW20_SERIAL.available()) {
-      int c = LW20_SERIAL.read();
-      USER_SERIAL.write(c); //Debug: print raw response to Serial Monitor
-      
-      if (c == '\n') {
-        responseData[responseDataSize] = 0;
-        USER_SERIAL.print("Received: ");
-        USER_SERIAL.println(responseData);
-        return true;
-      }
-      else if (c != '\r') {
-        if (responseDataSize == sizeof(responseData) - 1) {
-          responseDataSize = 0;
-        }
-        
-        responseData[responseDataSize++] = (char)c;
-      }
-    }
-  }
-  USER_SERIAL.println("Command Timeout!");
-  return false;
-}
-
-//----------------------------------------------------------------------------------------------
-// Gets a number from the response string.
-// Most commands have a single number that needs to be extracted from the response. That number
-// occurs after the ':' character.
-//----------------------------------------------------------------------------------------------
-float getNumberFromResponse(char* ResponseStr) {
-  // Find the ':' character.
-  int index = 0;
+  // Request a distance.
+  Serial1.write("d\n");
   
-  while (ResponseStr[index] != 0) {    
-    if (ResponseStr[index] == ':') {
-      return atof(ResponseStr + index + 1);
-    }
-    ++index;
+  // Wait for distance response.
+  while(c != '\n') {
+    while (!Serial1.available());
+    c = Serial1.read();
+    
+    resultBuffer[bufferIndex++] = c;
   }
-  return -1.0f; // Indicate failure
-}
 
-//----------------------------------------------------------------------------------------------
-// Display the product info response.
-//----------------------------------------------------------------------------------------------
-void lw20PrintProductInfo() {  
-  if (lw20SendCommand("?\r\n")) {
-    USER_SERIAL.print("Got product response: ");
-    USER_SERIAL.println(responseData);
-  } else {
-    USER_SERIAL.println("Could not get product response");
+  // Process response into distance value.
+  resultBuffer[bufferIndex - 2] = 0;
+  float distance = atof(resultBuffer);
+  
+  Serial.print(distance);
+  Serial.println(" m");
+  
+  delay(100);  
+
+  if (distance < 1) {
+    analogWrite(motorForward, 200);
+    delay(5000);
+    analogWrite(motorForward, 0);
+    delay(2000);
+    analogWrite(motorReverse, 200);
+    delay(5000);
+    analogWrite(motorReverse, 0);
+    delay(500);
   }
-}
-
-//----------------------------------------------------------------------------------------------
-// Get the current distance.
-//----------------------------------------------------------------------------------------------
-float lw20GetDistance() {
-  // NOTE: LDL = median distance to last return.
-  if (lw20SendCommand("?ldl\r\n"))
-    return getNumberFromResponse(responseData);
-
-  return 0.0f;
-}
-
-//----------------------------------------------------------------------------------------------
-// Get the signal strength.
-//----------------------------------------------------------------------------------------------
-float lw20GetStrength() {
-  if (lw20SendCommand("?lh\r\n"))
-    return getNumberFromResponse(responseData);
-
-  return 0.0f;
 }
